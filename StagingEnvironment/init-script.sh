370ec7225116d9863915a8616d145210b8fd4247
Las-Mohamed <airscal@gmail.com>
2023-05-31 16:24:20 +0200

    V3

--- StagingEnvironment/init-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install curl
-#Installing Docker
-sudo curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-#Creating container
-sudo docker run -d -p 80:80 --name last mowqa/last-nginx
